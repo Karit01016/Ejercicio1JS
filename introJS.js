a59e1b1aeb75a85ddb04cda6e92b1846ee7514a6
Karen Zarate <krenz06@hotmail.com>
2023-11-01 22:00:04 -0500

    final

---
+++ introJS.js
@@ -0,0 +1,177 @@
+// let miNombre = "Karen"
+// let miApellido = "Zarate"
+// let miEdad = 32
+// let miMascota = "Yiya"
+// let edadMascota = 5
+// console.log(miNombre)
+// console.log(miApellido)
+// console.log(miEdad)
+// console.log(miMascota)
+// console.log(edadMascota)
+
+/* 1 manera de hacer el punto 10*/
+/*
+let miNombre = prompt("Ingresar nombre")
+  let miApellido = prompt("Ingresar apellido")
+  let miEdad = prompt("Ingresar edad")
+  let miMascota = prompt("Ingresar nombre mascota")
+  let edadMascota = prompt("Ingresar edad mascota")
+
+
+  
+  let nombreCompleto = miNombre +" "+ miApellido
+
+  let textoPresentacion = `Hola, soy ${nombreCompleto}, tengo ${miEdad} años y mi mascota se llama ${miMascota} y tiene ${edadMascota} años.`;
+
+
+
+//  let textoPresentacion = miNombre + miApellido + miEdad + miMascota + edadMascota
+//  console.log(textoPresentacion);
+  let sumaEdades = miEdad + edadMascota 
+ 
+  let restaEdades = miEdad - edadMascota 
+  
+  let productoEdades =miEdad * edadMascota 
+  
+  let divisionEdades = miEdad / edadMascota 
+
+
+
+/*  segunda manera de hacer el punto 10
+  
+console.log("nombreCompleto:", nombreCompleto);
+ console.log("textoPresentacion:", textoPresentacion);
+console.log("sumaEdades:", sumaEdades);
+console.log("restaEdades:", restaEdades);
+console.log("productoEdades:", productoEdades);
+console.log("divisionEdades:", divisionEdades)
+/* punto 11 
+let alumno = {
+    nombre: " Andres",
+    apellido: "Romero",
+    curso:" JavaScript",
+    apodo: "la vaca lola",
+    edad: 28
+}
+  console.table(alumno)
+
+  console.log(alumno.nombre)
+
+  console.log(alumno.apellido)
+  console.log(alumno.curso)
+  console.log(alumno.apodo)
+  console.log(alumno.edad)
+/* punto 12 
+  let Mascota = {
+    nombre: "Yiya",
+    comida: "Nutrecan",
+    vacuna:"Todas",
+    salidas: "6 veces al dia",
+    edad: 7
+}
+console.table(Mascota)*/ 
+
+// console.log(alumno.nombre)
+
+// console.log(alumno.comida)
+// console.log(alumno.vacuna)
+// console.log(alumno.salidas)
+// console.log(alumno.edad)
+
+/*punto 13 */
+// const frutas = [ "Mango","Pera","Kiwi","Banano","Fresas" ]
+// console.table(frutas)
+
+// console.log(frutas[0])
+// console.log(frutas[1])
+// console.log(frutas[2])
+// console.log(frutas[3])
+// console.log(frutas[4])
+
+/*punto 14 */
+// const numeros = [ "50","40","30","20","10" ]
+// console.table(numeros)
+
+// console.log(numeros[0])
+// console.log(numeros[1])
+// console.log(numeros[2])
+// console.log(numeros[3])
+// console.log(numeros[4])
+
+
+/*punto 15 */
+
+/*
+const familia = [ "mamà","papà","hermano","hermana","bebe" ]
+console.table(familia)
+
+console.log(familia[0])
+console.log(familia[1])
+console.log(familia[2])
+console.log(familia[3])
+console.log(familia[4])
+
+
+/*punto 16 */
+// let textoAleatorio = `La ${frutas[1]}, es la fruta mas rica el ${numeros[4]} es el cumpleaños de mi ${familia[5]} es una gran fiesta y le gustala fruta ${frutas[3]} el potacio, y las ${frutas[3]} con crema son buenas`;
+
+// console.log(textoAleatorio)
+
+
+/*punto 17 Utilizar prompt() para leer por pantalla mi edad y la edad de un compañero y
+mostrar por consola los resultados de comparar los valores y guardarlos en
+variables llamadas por ejemplo: edadesIguales, soyMayor, soyMenor, etc. Y
+mostrar mensajes en consola como los siguientes:
+a. Mi edad es igual a la de mi compañero: false
+b. Mi edad es mayor a la de mi compañero: true
+c. Mi edad es menor a la de mi compañero: false*/
+
+
+
+
+//  let Miedad =  prompt("Ingresa tu edad")
+//  let edadCompañero = prompt("escribe la edad de tu amigo")
+
+//  let edadesDesiguales = Miedad ===edadCompañero
+//  let soyMayor = Miedad > edadCompañero
+//  let soyMenor = Miedad < edadCompañero
+
+// console.log("Mi edad es igual a la de mi compañero: false")
+//  console.log("Mi edad es mayor a la de mi compañero: true")
+//  console.log("Mi edad es menor a la de mi compañero: false")
+//  console.log(`a. Mi edad es igual a la de mi compañero: ${edadesDesiguales}`);
+//  console.log(`b. Mi edad es mayor a la de mi compañero: ${soyMayor}`);
+//  console.log(`c. Mi edad es menor a la de mi compañero: ${soyMenor}`);
+/* 18. Compare su edad ingresada por pantalla con prompt() con el numero 18 y
+guardarlo en una variable llamada soyMayorDeEdad y mostrar por consola un
+mensaje que diga: Soy mayor de edad y el valor de la variable. */
+//  let edades = prompt(" INGRESE SU EDAD")
+//  let soyMayorDeEdad = edades >= 18
+ 
+//  alert("soy mayor de edad:"+soyMayorDeEdad)
+
+
+/* 
+19. Introducir por pantalla la edad y la altura de una persona y guardarlas en variables
+separadas y en una variable llamada puedeSubir el resultado de la operación
+resultante de si la persona es mayor de 6 años y además tiene una altura mínima*/
+
+//  let suEdad = prompt("Ingrese su edad")
+//  let suAltura= prompt("Ingrese su altura")
+//  let puedeSubir = (suEdad > 6 ) && ( suAltura >= 1.20)
+//  console.log("Puede subir" +" "+ puedeSubir)
+
+
+/*
+
+20. Introducir por pantalla el pase de una persona el cual puede ser “VIP”, “NORMAL”
+o “LIMITADO”, el saldo que dispone y guardarlos en variables separadas. En una
+variable llamada puedePasar guardar el resultado de la operación resultante de si
+la persona tiene pase “VIP” o si posee un saldo mayor a 1000. Mostrando un
+mensaje que diga: La persona puede pasar y el resultado de la variable. */
+//  let TipoDePase= prompt("Ingrese el tipo de pase que tiene VIP normal o limitado")
+//  let saldo = prompt("ingrese su saldo")
+
+//  let PuedePasar = (TipoDePase=="VIP") || (saldo >= 1000)
+
+//  console.log("Puede pasar",PuedePasar)
