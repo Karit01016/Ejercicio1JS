5176bb4415f850e2775424fb002986e79962ee40
Karen Zarate <krenz06@hotmail.com>
2023-11-01 21:23:51 -0500

    prueba 4

---
+++ pruebajs/main.js
@@ -0,0 +1,13 @@
+/*ejercicios condicionales*/
+ /*ejercicio 1 */
+ let numero1 = 2
+ let numero2 = 9
+ if (numero1 > numero2){
+    console.log("el numero uno si es mayor")
+ }
+/* EJERCICIO 2 */
+// console.log("resultado2",comparar2)
+// let numero3 = 2
+// let numero4 = 9
+// let comparacion =numero3>= numero4
+// console.log(comparacion)
